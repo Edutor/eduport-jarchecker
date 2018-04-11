47a02741280c5af54b74e070fe58016aaf85ce44
Anders Kalhauge <anders@kalhauge.dk>
2018-04-11 22:40:38 +0200

    Initial Commit

---
+++ src/main/kotlin/dk/edutor/eduport/jarchecker/JarChecker.kt
@@ -0,0 +1,9 @@
+package dk.edutor.eduport.jarchecker
+
+import dk.edutor.eduport.Port
+
+class JarChecker : Port {
+  override fun sayHello() {
+    println("Hello from Jar Checker Port")
+    }
+  }
