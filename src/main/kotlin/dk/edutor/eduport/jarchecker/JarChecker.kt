86ecc681fae91fabe85b15629bfcf130ab686d02
Anders Kalhauge <anders@kalhauge.dk>
2018-04-17 09:24:18 +0200

    myMavenRepo

--- src/main/kotlin/dk/edutor/eduport/jarchecker/JarChecker.kt
+++ src/main/kotlin/dk/edutor/eduport/jarchecker/JarChecker.kt
@@ -1,9 +1,14 @@
 package dk.edutor.eduport.jarchecker
 
-import dk.edutor.eduport.Port
+import dk.edutor.eduport.*
 
 class JarChecker : Port {
+  override fun check(challenge: Challenge, solution: Solution): Assessment {
+    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
   override fun sayHello() {
     println("Hello from Jar Checker Port")
     }
+
   }
